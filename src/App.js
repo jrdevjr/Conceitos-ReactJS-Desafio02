d3c7d90828b95fc3e2be3b2c70d775725a9adfa6
Jurandir Junior <jurandir@lincode.com.br>
2020-08-24 21:22:54 -0300

    App do Desafio 03 Concluido

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
 
+import api from "./services/api";
+
 function App() {
+
+  const [ repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response =>{
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories',{
+      title:`Repositorio ${Date.now()}`
+    });
+
+    const repository = response.data;
+    setRepositories([ ...repositories, repository])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+  
+    const repos = repositories.filter((repository) => repository.id !== id);
+
+    const newRepository = [];
+
+    repositories.map(repository => {
+      if(repository.id !== id){
+        newRepository.push(repository);
+      }
+    })
+    setRepositories(newRepository);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+      {repositories.map(repository => (
+        <li key = {repository.id}>
+          {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+         
+
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+      ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
